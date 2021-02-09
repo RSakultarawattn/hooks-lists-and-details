078b81151f3209d5559fdff9a09c58bf60411ad9
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-08 17:13:49 -0800

    detail page

--- src/containers/DetailPage.jsx
+++ src/containers/DetailPage.jsx
@@ -1,12 +1,34 @@
 import  React, { Component, useState } from 'react';
 import fetch from 'superagent';
+import { getCharacters } from '../services/rickAndMortyApi';
 
 const DetailPage = () => {
   const [character, setCharacter] = useState([]);
-  const [Loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    getCharacters().then((character) => {
+      setCharacter(character);
+      setLoading(true);
+    });
+  }, []);
+
+  if(loading) return <h1>Loading</h1>;
+  return <CharacterList character={character} />;
+
 };
 
-const componentDidMount = ({ target }) => {
-    if(target.name === 'character') setCharacter(target.value);
+export default DetailPage;
+
+
     
-}
+
+        
+
+// componentDidMount = async() => {
+//     // eslint-disable-next-line max-len
+//     const response = await fetch(`https://rickandmortyapi.com/api/character/${this.props.match.params.id}`);
+//     console.log(response)
+//     this.setState({ character: response.body, Loading: false });
+//   }
