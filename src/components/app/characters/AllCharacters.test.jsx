3b99968bc9ca44882e6c06da3b790368759006e6
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-10 16:03:57 -0800

    all tests passing

---
+++ src/components/app/characters/AllCharacters.test.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen, waitFor, act } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import characters from '../../fixtures/CharactersById.json';
+import AllCharacters from './AllCharacters';
+
+const server = setupServer (
+  rest.get('https://rickandmortyapi.com/api/character/2', (req, res, ctx) => {
+    return res(ctx.json(characters));
+  })
+);
+
+describe('CharacterById container', () => {
+  act(async() => {beforeAll(() => server.listen());
+    afterAll(() => server.close());
+  });
+
+  it('fetches and displays a characters details', async() => {
+    await act(async() => {
+      render(
+        <MemoryRouter>
+          <AllCharacters match={{ params: { id: '2' } }} />
+        </MemoryRouter>);
+
+    });
+
+    
+
+    
+    
+    
+    return waitFor(() => {
+      screen.getByText('Morty Smith');
+      
+    });
+  });
+});
